071f0d14be8f6f3c9dd05958b059a2a15362ec82
김정현 <jhkim@legaltech.co.kr>
2025-01-20 17:16:42 +0900

    [studydrf] Tutorial_01 finished

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    n = input()
+    skyline = []
+    for _ in range(n):
+        skyline.append(list(map(int, input())))
+
+    skyline
+
+    sys.stdout.write(f"-1")
+
+solution()
