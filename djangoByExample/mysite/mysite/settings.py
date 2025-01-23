fe70779a9524ee1289a8e9d7548ec610c10ada7f
김정현 <jhkim@legaltech.co.kr>
2025-01-23 14:07:21 +0900

    [djangoByExample][Feat] Chapter_02__share_by_email

--- djangoByExample/mysite/mysite/settings.py
+++ djangoByExample/mysite/mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "b91352@gmail.com"
+EMAIL_HOST_PASSWORD = 'mtve fxkf sckc hyzf'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
