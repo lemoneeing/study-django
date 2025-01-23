bd75bc132175f38371025004603a9a9982da11d4
lemoneeing <jh2two@gmail.com>
2025-01-23 11:00:55 +0900

    [djangoByExample][Feat] Chapter_02__pagination

--- djangoByExample/mysite/blog/views.py
+++ djangoByExample/mysite/blog/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.objects.get(pk=id)
     # except Post.DoesNotExist:
     #     raise Http404("Post does not exist")
-    post = get_object_or_404(Post, pk=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request,
                   'blog/post/detail.html',
