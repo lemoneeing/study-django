f92cd477f8cc10df01951d221c4826473cae7862
lemoneeing <jh2two@gmail.com>
2025-01-23 15:38:35 +0900

    [djangoByExample][Feat] Chapter_03__comment

--- djangoByExample/mysite/blog/views.py
+++ djangoByExample/mysite/blog/views.py
@@ -1,11 +1,12 @@
 from django.http import Http404
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 
-from .models import Post
-from .forms import EmailForm
+from .models import Post, Comment
+from .forms import EmailForm, CommentForm
 
 # Create your views here.
 def post_list(request):
@@ -41,16 +42,22 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
 
+    comments = post.comments.filter(active=True)
+
+    form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
 
     if request.method == 'POST':
-        form = EmailForm(request.POST)
+        form = EmailForm(data=request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             #이메일 전송
@@ -65,3 +72,18 @@ def post_share(request, post_id):
     return render(request,
                   'blog/post/share.html',
                   {'post': post, 'form': form, 'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request,
+                  'blog/post/comment.html',
+                  {'post': post, 'form':form, 'comment': comment})
