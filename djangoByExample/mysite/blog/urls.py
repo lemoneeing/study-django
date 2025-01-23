a0dac26f3dfe6497e24950de436526fdd2020577
김정현 <jhkim@legaltech.co.kr>
2025-01-23 11:00:55 +0900

    [djangoByExample][Feat] Chapter_02__pagination

--- djangoByExample/mysite/blog/urls.py
+++ djangoByExample/mysite/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import post_list, post_detail
+from . import views
+
 app_name="blog"
 
 urlpatterns=[
-    path('', post_list, name='post_list'),
-    path('<int:id>/', post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- test.py
+++ test.py
@@ -2,13 +2,27 @@ import sys
 input = sys.stdin.readline
 
 def solution():
-    n = input()
-    skyline = []
-    for _ in range(n):
-        skyline.append(list(map(int, input())))
+    n = int(input())
+    ball_str = input().strip()
 
-    skyline
+    r_cnt = ball_str.count('R')
 
-    sys.stdout.write(f"-1")
+
+
+    r_right = r_cnt
+    r_left_move = r_cnt
+
+
+    b_cnt = ball_str.count('B')
+    b_right_move = b_cnt
+    b_left_move = b_cnt
+
+    # 양 끝에서 연속된 색상 별 공 카운팅: r_left, r_right, b_left, b_right
+    # 각 색상 별 공 카운팅
+    # 색 x 방향 별 이동 횟수 계산:
+    # r_to_l = r_cnt - r_left
+
+
+    sys.stdout.write(f"{min(r_move, b_move)}")
 
 solution()
