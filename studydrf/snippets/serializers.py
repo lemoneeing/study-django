087237958a350c201b878ba72efc1b09155f4666
lemoneeing <jh2two@gmail.com>
2025-02-05 17:31:30 +0900

    [studydrf] Tutorial_04__Authentication & Permission(WIP)

--- studydrf/snippets/serializers.py
+++ studydrf/snippets/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
@@ -21,3 +22,13 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.save()
         
         return instance
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all()) 
+    # 역참조 관계인 Snippet 을 User의 직렬화 필드에 포함하기 위해 명시적으로 선언(안하면 User 직렬화에서 snippet 은 빠지게 됨.)
+    # PrimaryKeyRelatedField 를 사용하면 직력화 후 snippets 을 Snippet 객체의 pk 로 표현한다.
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
--- studydrf/snippets/urls.py
+++ studydrf/snippets/urls.py
@@ -5,6 +5,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name="snippets"),
     path('snippet/<int:pk>', views.SnippetDetail.as_view(), name="snippet"),
+    path('users/', views.UserList.as_view(), name="users"),
+    path('user/<int:pk>/', views.UserDetail.as_view(), name="user"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns) # http://localhost:8080/snippets/1.json 처럼 화면에 response 를 표시할 형식을 결정할 수 있다.
