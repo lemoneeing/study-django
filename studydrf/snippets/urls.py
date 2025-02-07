30f4e9ce50eb8cc8b43c7696c16815d4b5b80c3e
김정현 <jhkim@legaltech.co.kr>
2025-02-07 16:57:52 +0900

    [studydrf] Tutorial_05__Relationships & Hyperlinked APIs

--- studydrf/snippets/urls.py
+++ studydrf/snippets/urls.py
@@ -3,10 +3,13 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view(), name="snippets"),
-    path('snippet/<int:pk>', views.SnippetDetail.as_view(), name="snippet"),
-    path('users/', views.UserList.as_view(), name="users"),
-    path('user/<int:pk>/', views.UserDetail.as_view(), name="user"),
+    path('', views.api_root),
+    path('snippets/', views.SnippetList.as_view(), name="snippet-list"),
+    path('snippet/<int:pk>/', views.SnippetDetail.as_view(), name="snippet-detail"),
+    path('snippet/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name="snippet-highlight"),
+    path('users/', views.UserList.as_view(), name="users-list"),
+    path('user/<int:pk>/', views.UserDetail.as_view(), name="user-detail"),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns) # http://localhost:8080/snippets/1.json 처럼 화면에 response 를 표시할 형식을 결정할 수 있다.
+urlpatterns = format_suffix_patterns(urlpatterns)
+# http://localhost:8080/snippets/1.json 처럼 화면에 response 를 표시할 형식을 결정할 수 있다.
