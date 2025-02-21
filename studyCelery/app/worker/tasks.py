6c726d0fd1e1f3662960d8d7bf48cc1ce95b091e
lemoneeing <jh2two@gmail.com>
2025-02-21 13:55:43 +0900

    [Celery] Signals

--- studyCelery/app/worker/tasks.py
+++ studyCelery/app/worker/tasks.py
@@ -1,8 +1,9 @@
 import time
 from celery import shared_task
+from celery.signals import task_prerun, task_postrun
 
 
-@shared_task(queue='celery', rate_limit='9/m')
+@shared_task(queue='celery')#, rate_limit='9/m')
 def add(x, y, msg=''):
     return f"{msg}{x+y}"
 
@@ -15,6 +16,21 @@ def dumb():
 @shared_task(queue='celery')
 def xsum(numbers):
     return sum(numbers)
+
+
+@task_prerun.connect
+def task_prerun_handler(sender, task_id, task, args, kwargs, **kwargs_extra):
+    print(f"Sender {sender} | sender_type: {type(sender)}")
+    print(f"Task {task} | task_id {task_id} | task_name: {task.name}")
+
+
+@task_postrun.connect
+def task_postrun_handler(sender, task_id, task, args, kwargs, retval, state, **kwargs_extra):
+    print(f"Sender: {sender} | Complete task: {task_id} | Return {retval} | Task State: {state}")
+
+
+def simulating_task_signal():
+    add.apply_async([4, 4])
 
 
 @shared_task(queue='celery')
